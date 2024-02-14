7c72cbdda4cf7532245a2a307b6fc222a057616e
kimz code <81085401+KimzCodes@users.noreply.github.com>
2024-02-14 17:12:15 +0200

    finish

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -11,8 +11,10 @@ const Categories = () => {
   );
 
   useEffect(() => {
-    dispatch(actGetCategories());
-  }, [dispatch]);
+    if (!records.length) {
+      dispatch(actGetCategories());
+    }
+  }, [dispatch, records]);
 
   const categoriesList =
     records.length > 0
