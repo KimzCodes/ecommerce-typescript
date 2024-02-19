d08cf34b1f3beef30bd85a88f7759197671d7519
kimz code <81085401+KimzCodes@users.noreply.github.com>
2024-02-19 22:59:00 +0200

    finish

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
 import ReactDOM from "react-dom/client";
 import AppRouter from "@routes/AppRouter";
 // redux
+import { store, persistor } from "@store/index";
 import { Provider } from "react-redux";
-import store from "@store/index";
+import { PersistGate } from "redux-persist/integration/react";
+
 // styles
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <AppRouter />
+    <PersistGate loading={null} persistor={persistor}>
+      <AppRouter />
+    </PersistGate>
   </Provider>
 );
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,39 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import categories from "./categories/categoriesSlice";
 import products from "./products/productsSlice";
 import cart from "./cart/cartSlice";
 
+const cartPersistConfig = {
+  key: "cart",
+  storage,
+  whitelist: ["items"],
+};
+
+const rootReducer = combineReducers({
+  categories,
+  products,
+  cart: cart,
+});
+
 const store = configureStore({
-  reducer: {
-    categories,
-    products,
-    cart,
-  },
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
@@ -16,4 +41,4 @@ export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
 
-export default store;
+export { store };
