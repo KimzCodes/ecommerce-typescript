01cc9a3e7a8216d16d8ef218a7a483ff09b47b9b
kimz code <81085401+KimzCodes@users.noreply.github.com>
2024-02-19 15:38:21 +0200

    finish

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,13 @@ import ReactDOM from "react-dom/client";
 import AppRouter from "@routes/AppRouter";
 // redux
 import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import { store, persistor } from "@store/index";
+import store from "@store/index";
 // styles
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <AppRouter />
-    </PersistGate>
+    <AppRouter />
   </Provider>
 );
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,54 +1,19 @@
-import { configureStore, combineReducers } from "@reduxjs/toolkit";
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from "redux-persist";
-import storage from "redux-persist/lib/storage";
-
+import { configureStore } from "@reduxjs/toolkit";
 import categories from "./categories/categoriesSlice";
 import products from "./products/productsSlice";
 import cart from "./cart/cartSlice";
 
-const rootPersistConfig = {
-  key: "root",
-  storage,
-};
-
-const cartPersistConfig = {
-  key: "cart",
-  storage,
-  blacklist: ["productFullInfo"],
-};
-
-const rootReducer = combineReducers({
-  categories,
-  products,
-  cart: persistReducer(cartPersistConfig, cart),
-});
-
-const persistedReducer = persistReducer(rootPersistConfig, rootReducer);
-
 const store = configureStore({
-  reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
+  reducer: {
+    categories,
+    products,
+    cart,
+  },
 });
-
-const persistor = persistStore(store);
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
 
-export { store, persistor };
+export default store;
