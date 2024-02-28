d773c4c69f527f6b7d5f15bba341c1b7284cf7d4
kimz code <81085401+KimzCodes@users.noreply.github.com>
2024-02-28 17:00:55 +0200

    fixes

--- src/components/eCommerce/Product/Product.tsx
+++ src/components/eCommerce/Product/Product.tsx
@@ -1,69 +1,67 @@
-import { useEffect, useState } from "react";
-import { useAppDispatch, useAppSelector } from "@store/hooks";
-import {
-  addToCart,
-  itemQuantityAvailabilityCheckingSelector,
-} from "@store/cart/cartSlice";
+import { useEffect, useState, memo } from "react";
+
 import { Button, Spinner } from "react-bootstrap";
 import { TProduct } from "@customTypes/product";
 
 import styles from "./styles.module.css";
 const { product, productImg, maximumNotice } = styles;
 
-const Product = ({ id, title, price, img, max }: TProduct) => {
-  const dispatch = useAppDispatch();
-  const [isBtnDisabled, setIsBtnDisabled] = useState(false);
+const Product = memo(
+  ({
+    id,
+    title,
+    price,
+    img,
+    max,
+    quantity,
+    addToCartHandler,
+  }: TProduct & { addToCartHandler?: (id: number) => void }) => {
+    const [isBtnDisabled, setIsBtnDisabled] = useState(false);
 
-  const { currentRemainingQuantity, quantityReachedToMax } = useAppSelector(
-    (state) =>
-      itemQuantityAvailabilityCheckingSelector(state.cart.items[id], max)
-  );
+    const currentRemainingQuantity = quantity ? max - quantity : max - 0;
+    const quantityReachedToMax = currentRemainingQuantity <= 0 ? true : false;
 
-  useEffect(() => {
-    if (!isBtnDisabled) {
-      return;
-    }
+    useEffect(() => {
+      if (!isBtnDisabled) {
+        return;
+      }
 
-    const debounce = setTimeout(() => {
-      setIsBtnDisabled(false);
-    }, 300);
+      const debounce = setTimeout(() => {
+        setIsBtnDisabled(false);
+      }, 300);
 
-    return () => clearTimeout(debounce);
-  }, [isBtnDisabled]);
+      return () => clearTimeout(debounce);
+    }, [isBtnDisabled]);
 
-  const addToCartHandler = () => {
-    dispatch(addToCart(id));
-    setIsBtnDisabled(true);
-  };
-
-  return (
-    <div className={product}>
-      <div className={productImg}>
-        <img src={img} alt={title} />
+    return (
+      <div className={product}>
+        <div className={productImg}>
+          <img src={img} alt={title} />
+        </div>
+        <h2>{title}</h2>
+        <h3>{price.toFixed(2)} EGP</h3>
+        <p className={maximumNotice}>
+          {quantityReachedToMax
+            ? "You reached to the limit"
+            : `You can add ${currentRemainingQuantity} item(s)`}
+        </p>
+        <Button
+          variant="info"
+          style={{ color: "white" }}
+          onClick={() => addToCartHandler && addToCartHandler(id)}
+          disabled={isBtnDisabled || quantityReachedToMax}
+        >
+          {isBtnDisabled ? (
+            <>
+              <Spinner animation="border" size="sm" /> Loading...
+            </>
+          ) : (
+            "Add to cart"
+          )}
+        </Button>
       </div>
-      <h2>{title}</h2>
-      <h3>{price.toFixed(2)} EGP</h3>
-      <p className={maximumNotice}>
-        {quantityReachedToMax
-          ? "You reached to the limit"
-          : `You can add ${currentRemainingQuantity} item(s)`}
-      </p>
-      <Button
-        variant="info"
-        style={{ color: "white" }}
-        onClick={addToCartHandler}
-        disabled={isBtnDisabled || quantityReachedToMax}
-      >
-        {isBtnDisabled ? (
-          <>
-            <Spinner animation="border" size="sm" /> Loading...
-          </>
-        ) : (
-          "Add to cart"
-        )}
-      </Button>
-    </div>
-  );
-};
+    );
+  }
+);
 
 export default Product;
--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,10 +1,11 @@
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "@store/hooks";
 import {
   actGetProductsByCatPrefix,
   productsCleanUp,
 } from "@store/products/productsSlice";
+import { addToCart } from "@store/cart/cartSlice";
 import { GridList, Heading } from "@components/common";
 import { Product } from "@components/eCommerce";
 import { Loading } from "@components/feedback";
@@ -14,6 +15,7 @@ const Products = () => {
   const params = useParams();
   const dispatch = useAppDispatch();
   const { loading, error, records } = useAppSelector((state) => state.products);
+  const items = useAppSelector((state) => state.cart.items);
 
   useEffect(() => {
     dispatch(actGetProductsByCatPrefix(params.prefix as string));
@@ -23,13 +25,27 @@ const Products = () => {
     };
   }, [dispatch, params]);
 
+  const recordsFullInfo = records.map((el) => ({
+    ...el,
+    quantity: items[el.id] || 0,
+  }));
+
+  const addToCartHandler = useCallback(
+    (id: number) => {
+      dispatch(addToCart(id));
+    },
+    [dispatch]
+  );
+
   return (
     <>
       <Heading>{params.prefix?.toUpperCase()} Products</Heading>
       <Loading status={loading} error={error}>
         <GridList<TProduct>
-          records={records}
-          renderItem={(record) => <Product {...record} />}
+          records={recordsFullInfo}
+          renderItem={(record) => (
+            <Product {...record} addToCartHandler={addToCartHandler} />
+          )}
         />
       </Loading>
     </>
