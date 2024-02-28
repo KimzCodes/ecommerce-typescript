8d145ce7918f6c7a81a6036519d307ff13d06535
kimz code <81085401+KimzCodes@users.noreply.github.com>
2024-02-28 21:34:30 +0200

    finish

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,11 +1,10 @@
-import { useCallback, useEffect } from "react";
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "@store/hooks";
 import {
   actGetProductsByCatPrefix,
   productsCleanUp,
 } from "@store/products/productsSlice";
-import { addToCart } from "@store/cart/cartSlice";
 import { GridList, Heading } from "@components/common";
 import { Product } from "@components/eCommerce";
 import { Loading } from "@components/feedback";
@@ -15,7 +14,6 @@ const Products = () => {
   const params = useParams();
   const dispatch = useAppDispatch();
   const { loading, error, records } = useAppSelector((state) => state.products);
-  const items = useAppSelector((state) => state.cart.items);
 
   useEffect(() => {
     dispatch(actGetProductsByCatPrefix(params.prefix as string));
@@ -25,27 +23,13 @@ const Products = () => {
     };
   }, [dispatch, params]);
 
-  const recordsFullInfo = records.map((el) => ({
-    ...el,
-    quantity: items[el.id] || 0,
-  }));
-
-  const addToCartHandler = useCallback(
-    (id: number) => {
-      dispatch(addToCart(id));
-    },
-    [dispatch]
-  );
-
   return (
     <>
       <Heading>{params.prefix?.toUpperCase()} Products</Heading>
       <Loading status={loading} error={error}>
         <GridList<TProduct>
-          records={recordsFullInfo}
-          renderItem={(record) => (
-            <Product {...record} addToCartHandler={addToCartHandler} />
-          )}
+          records={records}
+          renderItem={(record) => <Product {...record} />}
         />
       </Loading>
     </>
