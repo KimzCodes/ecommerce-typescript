6cd7c3f005831cd95b95f48efd5435e1affe6199
kimz code <81085401+KimzCodes@users.noreply.github.com>
2024-03-06 18:11:16 +0200

    fix

--- src/store/wishlist/act/actToggleLike.ts
+++ src/store/wishlist/act/actToggleLike.ts
@@ -5,7 +5,24 @@ const actToggleLike = createAsyncThunk(
   "wishlist/actToggleLike",
   async (id: number, thunkAPI) => {
     const { rejectWithValue } = thunkAPI;
-    const;
+    try {
+      const wishlistItems = await axios.get<{ id: number }[]>(
+        "/wishlist?userId=1"
+      );
+      const wishlistItemsId = wishlistItems.data.map((el) => el.id);
+      const idIsExist = wishlistItemsId.includes(id);
+
+      if (idIsExist) {
+        const userWishlist = await axios.get("/wishlist?userId=1");
+        await axios.delete("wishlist/");
+      } else {
+        await axios.post("wishlist", {
+          userId: 1,
+          itemId: id,
+        });
+        return { id, type: "add" };
+      }
+    } catch (error) {}
   }
 );
 
